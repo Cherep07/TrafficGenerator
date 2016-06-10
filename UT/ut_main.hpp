e6f528f852f73747671274b443d9d9f220a73a0b
tjhx37 <artyom.cherepanpov@nokia.com>
2016-06-10 19:00:27 +0300

    changes for ut

--- UT/ut_main.hpp
+++ UT/ut_main.hpp
@@ -1,5 +1,6 @@
 #include <cxxtest/TestSuite.h>
- 
+#include "main.hpp"
+
 class MyTestSuite : public CxxTest::TestSuite 
 {
 public:
@@ -7,5 +8,13 @@ public:
   {
 	  TS_ASSERT( 1 + 1 > 1 );
 	  TS_ASSERT_EQUALS( 1 + 1, 2 );
+  }
+  void testTrue(void)
+  {
+	  TS_ASSERT_EQUALS(5, dummy_function());
+  }
+  void testFalse(void)
+  {
+	  TS_ASSERT_EQUALS(4, dummy_function());
   }
 };
